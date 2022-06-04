0d3af1e7441f9f2d2aee9ec916208e41efebd33b
cosho <rizeupbass@gmail.com>
2022-06-04 23:40:36 +0900

    test2

--- Program.cs
+++ Program.cs
@@ -22,15 +22,16 @@ namespace VSMarketplaceBadges
                 .MinimumLevel.Information()
                 .Enrich.FromLogContext();
             string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            if (env == Microsoft.Extensions.Hosting.Environments.Development)
-                logConf.WriteTo.Console(new RenderedCompactJsonFormatter());
-            else if (env == Microsoft.Extensions.Hosting.Environments.Production)
-            {
-                logConf.WriteTo.Console(new RenderedCompactJsonFormatter());
-                AmazonS3(logConf.WriteTo, "logs", "vsmarketplcae-badges", RegionEndpoint.APNortheast1);
-            }
+            // if (env == Microsoft.Extensions.Hosting.Environments.Development)
+            //     logConf.WriteTo.Console(new RenderedCompactJsonFormatter());
+            // else if (env == Microsoft.Extensions.Hosting.Environments.Production)
+            // {
+            //     logConf.WriteTo.Console(new RenderedCompactJsonFormatter());
+            //     AmazonS3(logConf.WriteTo, "logs", "vsmarketplcae-badges", RegionEndpoint.APNortheast1);
+            // }
+            logConf.WriteTo.Console(new RenderedCompactJsonFormatter());
             Log.Logger = logConf.CreateLogger();
-
+            Log.Information($"env:{env}");
             try
             {
                 CreateHostBuilder(args).Build().Run();
